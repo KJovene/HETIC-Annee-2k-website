8f38cffc9db2a784b2b3630db547b1573a83b56c
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-01-21 15:52:18 +0100

    Router setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,52 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+import HomePage from './vues/HomePage'
+import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
+
+const Layout = () => {
+  console.log('  /\\_/\\  (\n ( ^.^ ) _)I ❤️ CATS\n   \\"/  ( \n ( | | )\n(__d b__)')
+  return (
+    <>
+      <header>
+       <Navbar />
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+          <Layout />
+    ),
+    children: [
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+      {
+        path: '/markdown',
+        element: <>Markdown</>,
+      },
+      {
+        path: '/recette',
+        element: <>Recette</>,
+      },
+      {
+        path: "/mocktail",
+        element: <>Mocktail</>
+      }
+    ],
+  }
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router} />
 )

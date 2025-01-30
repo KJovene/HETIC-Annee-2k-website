ad1694b1d2d42a5d75d45b216864f49249512776
Kévin <jovene.kevin@gmail.com>
2025-01-30 17:11:48 +0100

    Ajout des ingrédients et des photos

--- React2k/src/App.jsx
+++ React2k/src/App.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
-  const [drinks, setDrinks] = useState([]);
+  const [drink, setDrink] = useState(null);
 
   useEffect(() => {
+    // Fetch a random non-alcoholic drink
     fetch(
       "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic"
     )
@@ -11,7 +12,17 @@ function App() {
       .then((data) => {
         const randomDrink =
           data.drinks[Math.floor(Math.random() * data.drinks.length)];
-        setDrinks([randomDrink]);
+        return randomDrink.idDrink;
+      })
+      .then((idDrink) => {
+        // Fetch the details of the selected drink
+        return fetch(
+          "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + idDrink
+        );
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        setDrink(data.drinks[0]);
       })
       .catch((error) =>
         console.error("Erreur lors de la récupération des données :", error)
@@ -20,16 +31,27 @@ function App() {
 
   return (
     <div>
-      <ul>
-        {drinks.map((drink) => (
-          <p key={drink.idDrink}>
-            {drink.strDrink}
-            <br />
-            <img src={drink.strDrinkThumb} alt={drink.strDrink} width={200} />
-            <br />
-          </p>
-        ))}
-      </ul>
+      {drink && (
+        <div>
+          <h1>{drink.strDrink}</h1>
+          <img src={drink.strDrinkThumb} alt={drink.strDrink} width={200} />
+          <ul>
+            {Object.keys(drink)
+              .filter((key) => key.startsWith("strIngredient") && drink[key])
+              .map((key) => (
+                <li key={key}>
+                  {drink[key]}: {drink[`strMeasure${key.slice(13)}`]}
+                  <br />
+                  <img
+                    src={`https://www.thecocktaildb.com/images/ingredients/${drink[key]}-Small.png`}
+                    alt={drink[key]}
+                    width={70}
+                  />
+                </li>
+              ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }

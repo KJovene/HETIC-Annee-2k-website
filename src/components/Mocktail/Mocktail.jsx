477a07a0685d8aa55ff7f8052f4e918ab6f19780
Kévin <jovene.kevin@gmail.com>
2025-02-10 18:20:39 +0100

    Terminée

--- src/components/Mocktail/Mocktail.jsx
+++ src/components/Mocktail/Mocktail.jsx
@@ -36,47 +36,55 @@ function Mocktail() {
   }, [drink]);
 
   return (
-  <section className={styles.container}>
-    <div className={styles.containerMocktail}>
-      {drink && (
-        <div className={styles.HeadMocktail}>
-          <section className={styles.containerPoints}>
-          <div className={styles.Points}></div>
-          <div className={styles.Points}></div>
-          </section>
-          <section className={styles.Head}>
-            <section className={styles.titreMocktail}>
-              <h1 className={styles.NomMocktail}>{drink.strDrink}</h1>
-              <img
-                className={styles.imgMocktail}
-                src={drink.strDrinkThumb}
-                alt={drink.strDrink}
-                width={200}
-              />
+    <section className={styles.container}>
+      <div className={styles.containerMocktail}>
+        {drink && (
+          <div className={styles.HeadMocktail}>
+            <section className={styles.containerPoints}>
+              <div className={styles.Points}></div>
+              <div className={styles.Points}></div>
             </section>
-          </section>
-          <br />
-          <div>
-            <section>You will need :</section>
+            <section className={styles.Head}>
+              <section className={styles.titreMocktail}>
+                <h1 className={styles.NomMocktail}>{drink.strDrink}</h1>
+                <img
+                  className={styles.imgMocktail}
+                  src={drink.strDrinkThumb}
+                  alt={drink.strDrink}
+                  width={200}
+                />
+              </section>
+            </section>
             <br />
-            {Object.keys(drink)
-              .filter((key) => key.startsWith("strIngredient") && drink[key])
-              .map((key) => (
-                <p key={key} className={styles.ingredients}>
-                  {drink[key]}: {drink[`strMeasure${key.slice(13)}`]}
-                  <img
-                    className={styles.imgMocktailIngredient}
-                    src={`https://www.thecocktaildb.com/images/ingredients/${drink[key]}-Small.png`}
-                    alt={drink[key]}
-                    width={70}
-                  />
-                </p>
-              ))}
+            <div>
+              <p className={styles.need}>You will need :</p>
+              <br />
+              {Object.keys(drink)
+                .filter((key) => key.startsWith("strIngredient") && drink[key])
+                .map((key) => (
+                  <p key={key} className={styles.ingredients}>
+                    {drink[key]}: {drink[`strMeasure${key.slice(13)}`]}
+                    <img
+                      className={styles.imgMocktailIngredient}
+                      src={`https://www.thecocktaildb.com/images/ingredients/${drink[key]}-Small.png`}
+                      alt={drink[key]}
+                      width={70}
+                    />
+                  </p>
+                ))}
+            <hr />
+            </div>
+            <br />
+            <div className={styles.preparation}>
+              <p className={styles.instructions}>Instructions :</p>
+              <br />
+              <p className={styles.bodyInstructions}>{drink.strInstructions}</p>
+              <br />
+            </div>
           </div>
-        </div>
-      )}
-    </div>
-  </section>
+        )}
+      </div>
+    </section>
   );
 }
 

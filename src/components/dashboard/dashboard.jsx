99bd6f2c3dbe8cec87e22f2653cca53a300e69de
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-02-15 00:00:39 +0100

    Do not pay attention to this

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import style from './dashboard.module.css'
 
 function Dashboard() {
--- src/main.jsx
+++ src/main.jsx
@@ -45,7 +45,7 @@ const router = createBrowserRouter([
       },
       {
         path: "/mocktail",
-        element: <>Mocktail</>
+        element: <MocktailPage/>
       },
       {
         path: "/dashboard",
